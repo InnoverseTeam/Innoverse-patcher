c50da51d9fd9bda517ecb3e477f3d265dc2f681d
cedkeChat01 <123301168+00cedke@users.noreply.github.com>
2024-05-21 19:35:42 +0200

    Update config.h

--- src/config.h
+++ src/config.h
@@ -5,13 +5,10 @@ class Config {
 public:
     static void Init();
 
-    // wups config items
     static bool connect_to_network;
 
-    // private stuff
     static bool need_relaunch;
     
-    // private stuff
     static bool is_wiiu_menu;
 
     static bool unregister_task_item_pressed;
