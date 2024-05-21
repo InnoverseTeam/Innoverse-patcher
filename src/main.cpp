033b01279f0e990b7017e7a7e21a92d865bbc596
cedkeChat01 <123301168+00cedke@users.noreply.github.com>
2024-05-21 19:36:24 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,3 @@
-/*  Copyright 2022 Pretendo Network contributors <pretendo.network>
-    Copyright 2022 Ash Logan <ash@heyquark.com>
-    Copyright 2019 Maschell
-
-    Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby
-    granted, provided that the above copyright notice and this permission notice appear in all copies.
-
-    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL
-    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
-    INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
-    IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-    PERFORMANCE OF THIS SOFTWARE.
-*/
-
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -43,10 +29,6 @@
 
 #include <gx2/surface.h>
 
-/**
-    Mandatory plugin information.
-    If not set correctly, the loader will refuse to use the plugin.
-**/
 WUPS_PLUGIN_NAME("Innoverse");
 WUPS_PLUGIN_DESCRIPTION("Innoverse Patcher");
 WUPS_PLUGIN_VERSION("v1.1");
@@ -60,7 +42,6 @@ WUPS_USE_WUT_DEVOPTAB();
 #include <mocha/mocha.h>
 #include <function_patcher/function_patching.h>
 
-//thanks @Gary#4139 :p
 static void write_string(uint32_t addr, const char* str)
 {
     int len = strlen(str) + 1;
@@ -104,7 +85,6 @@ INITIALIZE_PLUGIN() {
         DEBUG_FUNCTION_LINE("NotificationModule_InitLibrary failed");
     }
 
-    //get os version
     MCPSystemVersion os_version;
     int mcp = MCP_Open();
     int ret = MCP_GetSystemVersion(mcp, &os_version);
