a4e8b09f5a4fb23ade31f2e935647713a309d792
cedkeChat01 <123301168+00cedke@users.noreply.github.com>
2024-05-21 19:36:38 +0200

    Update replace_mem.cpp

--- src/utils/replace_mem.cpp
+++ src/utils/replace_mem.cpp
@@ -1,17 +1,3 @@
-/*  Copyright 2023 Pretendo Network contributors <pretendo.network>
-    Copyright 2023 Ash Logan <ash@heyquark.com>
-    Copyright 2019 Maschell
-
-    Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby
-    granted, provided that the above copyright notice and this permission notice appear in all copies.
-
-    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL
-    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
-    INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
-    IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-    PERFORMANCE OF THIS SOFTWARE.
-*/
-
 #include "replace_mem.h"
 #include "utils/logger.h"
 
@@ -34,7 +20,6 @@ bool replace(uint32_t start, uint32_t size, const char* original_val, size_t ori
 }
 
 void replaceBulk(uint32_t start, uint32_t size, std::span<const replacement> replacements) {
-    // work out the biggest input replacement
     auto max_sz = std::max_element(replacements.begin(), replacements.end(), [](auto& a, auto& b) {
         return a.orig.size_bytes() < b.orig.size_bytes();
     })->orig.size_bytes();
@@ -56,7 +41,7 @@ void replaceBulk(uint32_t start, uint32_t size, std::span<const replacement> rep
                         replacement.repl.size_bytes()
                 );
                 counts[i]++;
-                break; // don't check the other replacements
+                break; 
             }
         }
     }
