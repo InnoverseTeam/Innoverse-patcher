aa18e62c4a714ab6d62ae5ed722fc04b9fcaf0e3
cedkeChat01 <123301168+00cedke@users.noreply.github.com>
2024-06-14 19:50:10 +0200

    Update olv_urls.h

--- src/patches/olv_urls.h
+++ src/patches/olv_urls.h
@@ -3,7 +3,7 @@
 #include <cstdlib>
 
 const char original_url[] = "discovery.olv.nintendo.net/v1/endpoint";
-const char new_url[] =      "innoverse.alwaysdata.net/olv.portal/communities.ejs";
+const char new_url[] =      "innoverse.alwaysdata.net/olv/portal/";
 
 _Static_assert(sizeof(original_url) > sizeof(new_url),
                "new_url too long! Must be less than 38chars.");
