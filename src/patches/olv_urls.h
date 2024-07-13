b940f24d5a866fad3af39d4da72a9b32120953da
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-07-13 13:10:50 +0200

    Update olv_urls.h

--- src/patches/olv_urls.h
+++ src/patches/olv_urls.h
@@ -3,7 +3,7 @@
 #include <cstdlib>
 
 const char original_url[] = "discovery.olv.nintendo.net/v1/endpoint";
-const char new_url[] =      "inno-disco.alwaysdata.net/v1/endpoint";
+const char new_url[] =      "disc-inno.alwaysdata.net/v1/endpoint";
 
 _Static_assert(sizeof(original_url) > sizeof(new_url),
                "new_url too long! Must be less than 38chars.");
