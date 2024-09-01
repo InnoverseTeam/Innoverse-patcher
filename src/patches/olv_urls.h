de920881123e9982e0342e50e2da1a32c58a41da
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-09-01 17:51:26 +0200

    Update olv_urls.h

--- src/patches/olv_urls.h
+++ src/patches/olv_urls.h
@@ -16,7 +16,7 @@
 #include <cstdlib>
 
 const char original_url[] = "discovery.olv.nintendo.net/v1/endpoint";
-const char new_url[] =      "discovery.innoverse.club/v1/endpoint";
+const char new_url[] =      "discovery.olv.innoverse.club/v1/endpoint";
 
 _Static_assert(sizeof(original_url) > sizeof(new_url),
                "new_url too long! Must be less than 38chars.");
