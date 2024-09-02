f2e12f93a5b3dad4fa7ee1698c2c845fe0307fe7
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-09-02 17:01:14 +0200

    Update iosu_url_patches.h

--- src/iosu_url_patches.h
+++ src/iosu_url_patches.h
@@ -36,7 +36,7 @@ static const URL_Patch url_patches[] = {
         {0xE229DE0C, "n.app.pretendo.cc"},
         //nim-boss .bss
         {0xE24B8A24, "https://api.innoverse.club/p01/policylist/1/1/UNK"}, //WWP
-        {0xE31930D4, "https://%s%saccount.pretendo.cc/v%u/api/"}
+        {0xE31930D4, "https://%s%saccount.innoverse.club/v%u/api/"}
 
 };
 
