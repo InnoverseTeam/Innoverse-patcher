702abb055ca337bff342fddaea8244485f62b840
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-09-11 18:21:17 +0200

    Update iosu_url_patches.h

--- src/iosu_url_patches.h
+++ src/iosu_url_patches.h
@@ -18,7 +18,7 @@ static const URL_Patch url_patches[] = {
         {0xE2282DB4, "https://ecs.wup.shop.pretendo.cc/ecs/services/ECommerceSOAP"},
         {0xE22830A0, "https://ecs.wup.shop.pretendo.cc/ecs/services/ECommerceSOAP"},
         {0xE22830E0, "https://nus.wup.shop.pretendo.cc/nus/services/NetUpdateSOAP"},
-        {0xE2299990, "nppl.innoverse.club"},
+        {0xE2299990, "nppl.app.innoverse.club"},
         {0xE229A600, "https://pls.wup.shop.pretendo.cc/pls/upload"},
         {0xE229A6AC, "https://npvk-dev.app.pretendo.cc/reports"},
         {0xE229A6D8, "https://npvk.app.pretendo.cc/reports"},
@@ -33,9 +33,9 @@ static const URL_Patch url_patches[] = {
         {0xE22B3F70, "https://ias.c.shop.pretendo.cc/ias/services/IdentityAuthenticationSOAP"},
         {0xE22B3FBC, "https://cas.c.shop.pretendo.cc/cas/services/CatalogingSOAP"},
         {0xE22B3FFC, "https://nus.c.shop.pretendo.cc/nus/services/NetUpdateSOAP"},
-        {0xE229DE0C, "n.app.pretendo.cc"},
+        {0xE229DE0C, "n.app.innoverse.club"},
         //nim-boss .bss
-        {0xE24B8A24, "https://nppl.innoverse.club/p01/policylist/1/1/UNK"},
+        {0xE24B8A24, "https://nppl.app.innoverse.club/p01/policylist/1/1/UNK"},
         {0xE31930D4, "https://%s%saccount.innoverse.club/v%u/api/"}
 
 };
