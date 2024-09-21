1ac6e2451764fea2dc5ecbfc2e0c5819640811c6
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-09-21 10:28:35 +0200

    Delete src directory

--- src/Notification.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <string_view>
-
-void ShowNotification(std::string_view notification);
--- src/utils/sysconfig.h
+++
@@ -1,8 +0,0 @@
-#ifndef INKAY_SYSCONFIG_H
-#define INKAY_SYSCONFIG_H
-
-#include <nn/swkbd.h>
-
-nn::swkbd::LanguageType get_system_language();
-
-#endif 
