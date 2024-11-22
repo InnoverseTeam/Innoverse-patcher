49dbe0dc8c0f186c823d866fd1988412cbc3638f
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-11-22 07:01:48 +0100

    Update sysconfig.h

--- src/utils/sysconfig.h
+++ src/utils/sysconfig.h
@@ -1,8 +1,16 @@
-#ifndef INKAY_SYSCONFIG_H
-#define INKAY_SYSCONFIG_H
+//
+// Created by ash on 9/04/24.
+//
+
+#ifndef INNOVERSE_SYSCONFIG_H
+#define INNOVERSE_SYSCONFIG_H
 
 #include <nn/swkbd.h>
+#include <coreinit/mcp.h>
 
 nn::swkbd::LanguageType get_system_language();
+const char * get_console_serial();
+MCPSystemVersion get_console_os_version();
+unsigned short get_console_peertopeer_port();
 
-#endif 
+#endif //INNOVERSE_SYSCONFIG_H
